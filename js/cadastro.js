52eda2ac955c98c88cb5d17e9e4525abbcc4ca0c
Dorffdaniel <danielcontabill@hotmail.com>
2026-01-04 15:05:28 +0000

    add validacao cadastro e login

---
+++ js/cadastro.js
@@ -0,0 +1,9 @@
+class Cadastro {
+    constructor(nome, ultimoNome, email, senha) {
+        this.nome = nome;
+        this.ultimoNome = ultimoNome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+let cadastro = [];
 
 function mostrarModal() {
 
@@ -16,18 +17,68 @@ function confirmarTermos() {
 
 }
 
+function cadastrarNovaConta() {
+    let nome = $("#name").val();
+    let ultimoNome = $("#lastname").val();
+    let email = $("#email").val();
+    let senha = $("#passCad").val();
+
+    if (!nome || !ultimoNome || !email || !senha) {
+        alerta("Erro", "preencha todos os campos", "warning");
+        return;
+    }
+
+    cadastro.push(new Cadastro(nome, ultimoNome, email, senha));
+
+    localStorage.setItem("conteudoCad", JSON.stringify(cadastro));
+
+    alerta("sucesso", "Cadastrado com sucesso", "success");
+
+    $("#name").val('')
+    $("#lastname").val('');
+    $("#email").val('');
+    $("#passCad").val('');
+}
+
+
+
+
 function entrar() {
     const nome = $("#user").val();
     const senha = $("#password").val();
 
-    if (!nome || !senha) {
+    if (!nome.trim() || !senha.trim()) {
         alerta("Atenção", "campos vazios", "warning");
         return;
     }
 
-    EncaminharUsuario(nome);
+
+    let nomeCad = JSON.parse(localStorage.getItem("conteudoCad"));
+    console.log(nomeCad)
+
+    if (nomeCad == null) {
+        if (nome || senha) {
+            alerta("Erro", "user invalido, melhor fazer um cadastro", "warning");
+            return;
+        }
+
+    }
+
+    nomeCad.forEach(el => {
+
+        if (nome == el.nome && senha == el.senha) {
+            EncaminharUsuario(nome);
+        } else {
+            alerta("Erro", "valores incorretos", "error");
+        }
+
+    });
+
 
 }
+
+
+
 
 function EncaminharUsuario(nome) {
     localStorage.setItem("nome", nome);
